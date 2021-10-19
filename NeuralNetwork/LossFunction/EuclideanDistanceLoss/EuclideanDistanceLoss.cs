ac291c1fa3960148c09d2bef344a62e141675c29
Lihogub Oleg <oligli@yandex.ru>
2021-10-19 18:43:02 +0300

    Added loss function

---
+++ NeuralNetwork/LossFunction/EuclideanDistanceLoss/EuclideanDistanceLoss.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace neural_networks_kubsu.NeuralNetwork.LossFunction.EuclideanDistanceLoss
+{
+    public class EuclideanDistanceLoss : ILossFunction
+    {
+        public double ComputeLoss(double[] currentOutput, double[] expectedOutput)
+        {
+            var s = 0.0;
+            foreach (var i in Enumerable.Range(0, currentOutput.Length))
+            {
+                s += Math.Pow(expectedOutput[i] - currentOutput[i], 2.0);
+            }
+            return Math.Sqrt(s);
+        }
+    }
+}
---
+++ NeuralNetwork/LossFunction/ILossFunction.cs
@@ -0,0 +1,7 @@
+﻿namespace neural_networks_kubsu.NeuralNetwork.LossFunction
+{
+    public interface ILossFunction
+    {
+        public double ComputeLoss(double[] currentOutput, double[] expectedOutput);
+    }
+}
